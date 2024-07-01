7e487de70ad633a56785c1346f04cc1ef3288837
JyothiswarAkkula <138545303+JyothiswarAkkula@users.noreply.github.com>
2024-07-01 16:47:10 +0530

    Add files via upload

---
+++ ArrayMethodsUsingClass.js
@@ -0,0 +1,23 @@
+class add
+{
+    
+method(){
+let  a=[1,2,3,4,5,6]
+let b=[7,,8,9,10]
+console.log(a)
+console.log(a.length)
+console.log(a.toString())
+console.log(a.pop(1))
+console.log(a)
+console.log(a.at(1))
+console.log(a.push(2))
+console.log(a)
+console.log(a.join("_"))
+console.log(a.concat(b))
+console.log(a)
+console.log(a.sort())
+}
+
+}
+jyo=new add()
+jyo.method()
