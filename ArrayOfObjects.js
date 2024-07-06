44cc6fa3727580545ba3edfdf81008972d63f8bb
JyothiswarAkkula <138545303+JyothiswarAkkula@users.noreply.github.com>
2024-07-06 14:39:19 +0530

    Add files via upload

---
+++ ArrayOfObjects.js
@@ -0,0 +1,18 @@
+
+//creating a array of objects
+let a=[{name:"Jyothiswar",age:"24",city:"chittoor"},
+    {name:"deva",age:"44",city:"bglor"},
+    {name:"RameshBabu",age:"52",city:"chennai"}
+]
+
+//print the value
+for(i of a)    
+{
+    console.log(i)
+}
+
+//print the index
+for(i in a)    
+    {
+        console.log(i)
+    }
